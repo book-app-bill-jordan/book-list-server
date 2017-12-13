d1361995a0fea3e1fac189838c847cf5e5859dfb
Jordan Van Ness <jordanwvn@gmail.com>
2017-12-13 10:14:52 -0800

    minor changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/api/v1/books', (request, response) => {
     client.query(`SELECT book_id, title, author, image_url FROM books;`)
     .then(result => response.send(result.rows))
     .catch(console.error);
-  });
+});
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
